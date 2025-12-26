3b4a0345ead8d4d63cd060ba93f343868abc0dd2
yxb123456cy <2186256471@qq.com>
2025-12-26 17:06:05 +0800

    refactor: :recycle: 删除无效文件

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.module.scss'
 const ConfigView: React.FC = () => {
     return (
         <div>
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './detail.module.scss'
+import './index.module.scss'
 const DetailView: React.FC = () => {
     return (
         <div>
